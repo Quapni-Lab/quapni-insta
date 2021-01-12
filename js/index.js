c6747317aee85339e8a44d48bb0c765fc571cf25
Tsai Yi Lin <andy6804tw@yahoo.com.tw>
2021-01-12 14:21:30 +0800

    add api

--- js/index.js
+++ js/index.js
@@ -1,4 +1,42 @@
-function capture(){
-    console.log('asdsad')
-    document.getElementById("input-capture").click();
+
+const inputCapture = document.getElementById('inputCapture');
+
+/**
+ * File 轉 base64
+ * @param {*} file 
+ */
+const toBase64 = file => new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => resolve(reader.result.split(",")[1]);
+    reader.onerror = error => reject(error);
+});
+
+
+function capture() {
+    inputCapture.click();
 }
+
+const sendPic = async () => {
+    var file = inputCapture.files[0];
+
+    const base64Image = await toBase64(file);
+    console.log(base64Image)
+
+    axios.post(`http://127.0.0.1:5000/swap`, {
+        image: base64Image
+    })
+        .then((response) => {
+            var dataObject = response.data;
+            // POST success
+            const responseImg = dataObject.result.split("'")[1];
+            // document.getElementById('image').src = `data:image/jpeg;base64,${responseImg}`;
+
+        },
+            (error) => {
+                var message = error.response.data.message;
+            }
+        );
+}
+
+inputCapture.addEventListener('change', sendPic, false);
